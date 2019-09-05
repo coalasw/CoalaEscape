024026ae462ac06cfa95943118068e3dc597f45b
leehyeongwoo <leehyeongwoo@gmail.com>
2019-09-06 00:22:16 +0900

    Add example code

---
+++ Example1/myRoom-4.js
@@ -0,0 +1,46 @@
+﻿room = game.createRoom("room", "배경-1.png") // 방 생성
+
+room.door = room.createObject("door", "문-오른쪽-닫힘.png") // 문 생성
+room.door.setWidth(136) // 크기 조절
+room.locateObject(room.door, 1049, 300) // 문 배치
+room.door.lock() // door 상태를 locked로 변경
+
+room.door.onClick = function() { // door를 클릭했을 때
+	if(room.door.isClosed()){ // door가 closed 상태이면
+		room.door.open() // door의 상태를 open으로 바꿈
+	} else if (room.door.isOpened()){ // door가 opened 상태이면
+		game.clear() // 게임 클리어
+	} else if (room.door.isLocked()){ // door가 locked 상태이면
+		printMessage("문이 잠겨있다") // 메시지 출력
+	}
+}
+
+room.door.onOpen = function() { // door 상태가 open으로 변경되면 실행
+	room.door.setSprite("문-오른쪽-열림.png") // 열린 문으로 변경
+}
+
+room.keypad = room.createObject("keypad", "숫자키-우.png") // 오브젝트 생성
+room.keypad.setWidth(50) // 크기 조절
+room.locateObject(room.keypad, 930, 250) // 위치 변경
+
+room.keypad.onClick = function() {
+	printMessage("올해는 몇 년도?")
+	showKeypad("number", "2019" , function(){ // 키패드 1 - 숫자4자리
+		room.door.unlock() // door의 잠금을 연다
+		printMessage("잠금장치가 열리는 소리가 들렸다.")
+	 })
+}
+
+room.shelf = room.createObject("shelf", "선반-좌.png")
+room.shelf.setWidth(460)
+room.locateObject(room.shelf, 250, 150)
+
+room.book = room.createObject("book", "책3-1.png")
+room.book.setWidth(80)
+room.locateObject(room.book, 100, 140)
+room.book.onClick = function() {
+	showImageViewer("종이.png", "책.txt"); // 이미지 출력
+}
+
+game.start(room) // 게임시작
+printMessage("방탈출에 오신 것을 환영합니다!") // 환영 메시지 출력
